2caf8e8fc1413ff502d0494474aaf69c9155490f
Mirko <73440834+MirkoVojinovicRA182@users.noreply.github.com>
2021-12-08 23:17:40 +0100

    feat: Creat users: Cottage owner, Ship owner, Instructor and subclass User

---
+++ demo/src/main/java/app/domain/ShipOwner.java
@@ -0,0 +1,39 @@
+package app.domain;
+
+import app.domain.enums.UserType;
+
+public class ShipOwner extends User{
+    private UserType userType;
+    private String registrationExplanation;
+
+    public ShipOwner() {
+    }
+
+    public ShipOwner(Long id, String email, String password, String name, String lastName, String adress, String city, String country, String phoneNumber, UserType userType, String registrationExplanation) {
+        super(id, email, password, name, lastName, adress, city, country, phoneNumber);
+        this.userType = userType;
+        this.registrationExplanation = registrationExplanation;
+    }
+
+    public ShipOwner(String email, String password, String name, String lastName, String adress, String city, String country, String phoneNumber, UserType userType, String registrationExplanation) {
+        super(email, password, name, lastName, adress, city, country, phoneNumber);
+        this.userType = userType;
+        this.registrationExplanation = registrationExplanation;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public String getRegistrationExplanation() {
+        return registrationExplanation;
+    }
+
+    public void setRegistrationExplanation(String registrationExplanation) {
+        this.registrationExplanation = registrationExplanation;
+    }
+}
---
+++ demo/src/main/java/app/domain/enums/UserType.java
@@ -0,0 +1,10 @@
+package app.domain.enums;
+
+public enum UserType {
+    CootageOwner,
+    ShipsOwner,
+    Instructor,
+    Client,
+    Administrator,
+    Unauthenticated
+}
