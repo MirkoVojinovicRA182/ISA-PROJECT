101830723f7ab022b33d3e27fdb5e0c9dd78ae05
Mirko <73440834+MirkoVojinovicRA182@users.noreply.github.com>
2021-12-26 13:31:52 +0100

    feat: Create side bar

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InstructorHomePageComponent } from './components/instructor-home-page/instructor-home-page/instructor-home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstructorHomePageComponent
   ],
   imports: [
     BrowserModule,
