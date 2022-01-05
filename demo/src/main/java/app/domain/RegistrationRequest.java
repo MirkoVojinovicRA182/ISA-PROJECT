954fac66e1331a3b74d10b90ed0aade29f669290
Mirko <73440834+MirkoVojinovicRA182@users.noreply.github.com>
2022-01-05 19:37:45 +0100

    feat: Change colors of admin registration component

--- demo/src/main/java/app/domain/RegistrationRequest.java
+++ demo/src/main/java/app/domain/RegistrationRequest.java
@@ -7,7 +7,35 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-public class RegistrationRequest extends ApplicationUser{
+public class RegistrationRequest{
+    @Id
+    @SequenceGenerator(name = "regReqSeqGen", sequenceName = "regReqSeq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "regReqSeqGen")
+    private Integer id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", unique = false, nullable = false)
+    private String password;
+
+    @Column(name = "name", unique = false, nullable = false)
+    private String name;
+
+    @Column(name = "lastName", unique = false, nullable = false)
+    private String lastName;
+
+    @Column(name = "address", unique = false, nullable = false)
+    private String address;
+
+    @Column(name = "city", unique = false, nullable = false)
+    private String city;
+
+    @Column(name = "country", unique = false, nullable = false)
+    private String country;
+
+    @Column(name = "phoneNumber", unique = true, nullable = false)
+    private String phoneNumber;
 
     @Column(name = "userType", unique = false, nullable = false)
     private UserType userType;
@@ -21,10 +49,76 @@ public class RegistrationRequest extends ApplicationUser{
     public RegistrationRequest() {
     }
 
-    public RegistrationRequest(UserType userType, String registrationExplanation, LocalDate requestDate) {
-        this.userType = userType;
-        this.registrationExplanation = registrationExplanation;
-        this.requestDate = requestDate;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public UserType getUserType() { return userType; }
--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,13 +1,3 @@
-insert into instructor (id, address, city, country, email, last_name, name, password, phone_number) values (1,
-                                                                                                            'Karađorđeva 12',
-                                                                                                            'Nova Gajdobra',
-                                                                                                            'Republika Srbija',
-                                                                                                            'mare@gmail.com',
-                                                                                                            'Marko',
-                                                                                                            'Isaković',
-                                                                                                            'mare123',
-                                                                                                            '065-244-32-21');
-
 insert into administrator (id, address, city, country, email, name, last_name, password, phone_number) values (2,
                                                                                                             'Vojvođanska 32',
                                                                                                             'Obrovac',
@@ -17,6 +7,46 @@ insert into administrator (id, address, city, country, email, name, last_name, p
                                                                                                             'Mitrović',
                                                                                                             'mitar567',
                                                                                                             '065-231-222-01');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+(1, 'maki@gmail.com', 'maki123', 'Marija', 'Stojadinović', 'Bulevar Mihajla Pupina 12', 'Novi Sad', 'Republika Srbija', '066/113-23-32', 1, 'Because.', '2022-01-20T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (2, 'mira@yahoo.com', 'mira02131', 'Mira', 'Jokić', 'Doža Đerđa', 'Novi Sad', 'Republika Srbija', '066/123-13-32', 2, 'Because.', '2022-01-22T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (3, 'nikolaM@gmail.com', 'manda123', 'Nikola', 'Mandić', 'Dr. Svetislava Kasapinovića', 'Novi Sad', 'Republika Srbija', '065/221-56-62', 0, 'Because.', '2022-01-18T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (4, 'jenja@gmail.com', 'jenja123', 'Jovan', 'Jenjić', 'Kosovska 5', 'Novi Sad', 'Republika Srbija', '061/173-89-382', 1, 'Because.', '2021-12-01T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (5, 'mili123@gmail.com', 'mili123', 'Strahinja', 'Milivojević', 'Železnička 11', 'Novi Sad', 'Republika Srbija', '061/113-2331-32', 0, 'Because.', '2021-02-01T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (6, 'joecole@gmail.com', 'joeCole4453', 'Joe', 'Cole', 'Bulevar Mihajla Pupina 12', 'Novi Sad', 'Republika Srbija', '066/1133-23-32', 1, 'Because.', '2021-02-01T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (7, 'leabrich@yahoo.com', 'lea0121', 'Lea', 'Brich', 'Vojvođanska 13', 'Bačka Topola', 'Republika Srbija', '066/1213-23-32', 1, 'Because.', '2021-02-01T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (8, 'oki@gmail.com', 'okiM212', 'Mitar', 'Okulić', 'Slobodana Selenića', 'Vrbas', 'Republika Srbija', '066/113-23-553', 0, 'Because.', '2021-02-01T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (9, 'nukitr321@gmail.com', '76767jfkgjd', 'Nikola', 'Trbojević', 'Pionirska 11', 'Mladenovo', 'Republika Srbija', '066/123-23-32', 2, 'Because.', '2021-02-01T08:00:00.00');
+
+insert into registration_request (id, email, password, name, last_name, address, city, country, phone_number, user_type, registration_explanation, request_date) values
+    (10, 'steva123@gmail.com', 'steva123', 'Stevan', 'Simić', 'Stevana Sremca 89', 'Novi Sad', 'Republika Srbija', '066/1123-23-32', 2, 'Because.', '2021-02-01T08:00:00.00');
+
+insert into instructor (id, address, city, country, email, last_name, name, password, phone_number) values (1,
+                                                                                                            'Karađorđeva 12',
+                                                                                                            'Nova Gajdobra',
+                                                                                                            'Republika Srbija',
+                                                                                                            'mare@gmail.com',
+                                                                                                            'Marko',
+                                                                                                            'Isaković',
+                                                                                                            'mare123',
+                                                                                                            '065-244-32-21');
 
 insert into instructor_adventure(id, name, address, promotional_description, instructor_biography, max_count_of_participants, rules_of_conduct, default_equipment, pricelist, terms_of_use, instructor_id)
  values (1, 'Pecanje u Obrovcu', 'Kanal DTD', 'Uživajte na pecanju u Obrovcu', 'Trenutno nedostupna', 10, 'Pravila', 'Pecaljka/Ručak', 'Cenovnik', 'Uslovi otkaza rezervacije', 1);
